cc5178698d041e08c282cf39998bccc003cd13a6
wisegps <thomas@wisegps.cn>
2017-02-23 18:01:48 +0800

    170223

---
+++ app/components/header.js
@@ -0,0 +1,59 @@
+import './header.css';
+import '../lib/swiper.min.css';
+import React from 'react';
+import Swiper from '../lib/swiper.min.js'
+import fetchJsonp from 'fetch-jsonp';
+
+let Header = React.createClass({
+    getInitialState: function() {
+        return {
+            imgUrls: [],
+        }
+    },
+    componentDidMount: function() {
+        fetchJsonp(this.props.source).then((response) => {
+            return response.json();
+        }).then((data) => {
+            if(data.status) {
+                if(this.isMounted()) {
+                    this.setState({
+                        imgUrl: data.data,
+                    })
+                    new Swiper('#header .swiper-container', {
+                        loop: true,
+                        pagination: 'swiper-pagination',
+                        paginationClickable: true,
+                        autoplay: 3000,
+                        autoplayDisableOnInteraction: false,
+                    })
+                }
+            }else {
+                alert(data.msg);
+            }
+        })
+    },
+    render: function () {
+        let countId = 0;
+        return (
+            <div id="header">
+                <div className="swiper-container">
+                    <div className="swiper-wrapper">
+                        {
+                            this.state.imgUrls.map((url) => {
+                            return <div className="swiper-slider" key={"header" + countId++} >
+                                        <img className="img" src={url} />
+                                    </div>
+                            })
+                        }
+                    </div>
+                    <div className="swiper-pagination"></div>
+                </div>
+            </div>
+        );
+    }
+})
+//Prop验证
+Header.propTypes = {
+    source: React.PropTypes.string.isRequired,
+}
+module.exports = Header;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webapck.loaders');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:8080',
+        './index.js'
+    ],
+    devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    //其它解决方案配置
+    resolve: {
+        extension: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: loaders
+    },
+    devServer: {
+        contentBase: "./build", //静态资源的目录
+        noInfo: true, //--no-info option
+        hot: true, //自动刷新
+        inline: true
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env':{
+                'NODE_ENV': JSON.stringify('production')
+        }
+        }),
+        new CopyWebpackPlugin([
+            {from: './index.html'}
+        ])
+    ]
+};
